df46c770f5c8726dc799ae231db83824f48ac971
Bill Zafeiris <bzafiris@gmail.com>
2026-03-26 10:45:59 +0200

    Fixed compilation error related to WebSecurityConfig

--- sb_tutorial_7_signup_signin/src/main/java/myy803/springboot/sb_tutorial_7_signup_signin/config/WebSecurityConfig.java
+++ sb_tutorial_7_signup_signin/src/main/java/myy803/springboot/sb_tutorial_7_signup_signin/config/WebSecurityConfig.java
@@ -1,21 +1,16 @@
 package myy803.springboot.sb_tutorial_7_signup_signin.config;
 
+import myy803.springboot.sb_tutorial_7_signup_signin.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import myy803.springboot.sb_tutorial_7_signup_signin.service.UserServiceImpl;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 
 /*
@@ -61,9 +56,7 @@ public class WebSecurityConfig {
      */
 	@Bean
     public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-        authProvider.setUserDetailsService(userDetailsService());
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
@@ -96,7 +89,7 @@ public class WebSecurityConfig {
                         );
                 
                 http.logout(logOut -> logOut.logoutUrl("/logout")
-                		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                		.logoutRequestMatcher(PathPatternRequestMatcher.withDefaults().matcher("/logout"))
                 		.logoutSuccessUrl("/")
                 		);
 
