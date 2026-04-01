0a9de9b7724c234fb054e929c00e666cf6b5e67c
Vassilis Zafeiris <bzafiris@uoi.gr>
2026-04-01 20:46:26 +0300

    [sb_tutorial_7] Added controller tests requiring authorization

---
+++ sb_tutorial_7_signup_signin/src/test/java/myy803/springboot/sb_tutorial_7_signup_signin/controller/AdminControllerTest.java
@@ -0,0 +1,58 @@
+package myy803.springboot.sb_tutorial_7_signup_signin.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application.properties")
+@AutoConfigureMockMvc
+class AdminControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    AdminController adminController;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser(authorities = {"ADMIN"})
+    @Test
+    public void showDashboardToAuthorizedUser() throws Exception {
+        mockMvc.perform(get("/admin/dashboard"))
+                .andExpect(status().isOk())
+                .andExpect(view()
+                        .name("admin/dashboard"));
+    }
+
+    @Test
+    public void showErrorToUnauthorizedUser() throws Exception {
+        mockMvc.perform(get("/admin/dashboard"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
+
+}
